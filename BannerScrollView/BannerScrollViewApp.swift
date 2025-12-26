f724153fd3d0d115d9a2d0f5260558b64d35fa5b
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-12-26 23:19:23 +0700

    Initial Commit

---
+++ BannerScrollView/BannerScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BannerScrollViewApp.swift
+//  BannerScrollView
+//
+//  Created by Iqbal Alhadad on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BannerScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
